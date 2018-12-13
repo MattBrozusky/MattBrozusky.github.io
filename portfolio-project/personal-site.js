59a70606ec427a995c9a4bddf6d2ffec2c355d0c
MattBrozusky <matthew.brozusky@gmail.com>
2018-12-13 16:56:10 -0600

    started filling in content for portfolio page

--- portfolio-project/personal-site.js
+++ portfolio-project/personal-site.js
@@ -1,36 +1,46 @@
 $(document).ready(() => {
 "use strict";
 
-
 //Nav pushpin call
     $('.pushpin').pushpin({
         top: $('.area').height(),
         bottom: $(document).height()
     });
 
-
 //Parallax call
     $('.parallax').parallax();
 
-
-
 //Scrolling animations
     $(window).bind('scroll', function() {
+        let downArrow = $('#down-arrow');
+        let about = $('#about');
+        let contact = $('#contact');
+        let portfolio = $('#portfolio');
         //Arrow
         if ($(window).scrollTop() > 100) {
-            $('#down-arrow').fadeOut(300);
+            downArrow.fadeOut(300);
         }
         else {
-            $('#down-arrow').fadeIn(300);
+            downArrow.fadeIn(300);
         }
-        
-        
+
         //Divider's
-        if ($(window).scrollTop() > ($(document).height() - $('#about').height())){
-            $('#about').fadeOut(500);
+        if ($(window).scrollTop() > about.offset().top - 530){
+            about.removeClass('invisible').addClass('slide-right');
         }
-        
-        
+
+        if ($(window).scrollTop() > portfolio.offset().top - 530){
+            portfolio.removeClass('invisible').addClass('slide-right');
+        }
+
+        if ($(window).scrollTop() > contact.offset().top - 530){
+            contact.removeClass('invisible').addClass('slide-right');
+        }
+
+        //Name Appear
+        if ($(window).scrollTop() > about.offset().top - 100){
+            $('#my-name').removeClass('invisible').addClass('fade-in');
+        }
     });
 
     function makeArrow() {
@@ -38,7 +48,4 @@ $(document).ready(() => {
         $('#down-arrow').css('top', headerHeight - 60);
     }
     makeArrow();
-
-    
-
 });
