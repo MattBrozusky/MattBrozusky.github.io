297e25e3c0ca0430f60569d5ac18d3b9fb6609ab
MattBrozusky <matthew.brozusky@gmail.com>
2018-12-14 16:53:50 -0600

    almost finished with content

--- portfolio-project/personal-site.js
+++ portfolio-project/personal-site.js
@@ -7,13 +7,24 @@ $(document).ready(() => {
         bottom: $(document).height()
     });
 
+//Carousel functionality
+    $('.carousel').carousel({
+        padding: 200,
+        pressed: false
+    });
+    autoplay();
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 3500);
+    }
+
 //Parallax call
     $('.parallax').parallax();
 
 //Scrolling animations
     $(window).bind('scroll', function() {
         let downArrow = $('#down-arrow');
-        let about = $('#about');
+        let biography = $('#biography');
         let contact = $('#contact');
         let portfolio = $('#portfolio');
         //Arrow
@@ -25,8 +36,8 @@ $(document).ready(() => {
         }
 
         //Divider's
-        if ($(window).scrollTop() > about.offset().top - 530){
-            about.removeClass('invisible').addClass('slide-right');
+        if ($(window).scrollTop() > biography.offset().top - 530){
+            biography.removeClass('invisible').addClass('slide-right');
         }
 
         if ($(window).scrollTop() > portfolio.offset().top - 530){
@@ -38,7 +49,7 @@ $(document).ready(() => {
         }
 
         //Name Appear
-        if ($(window).scrollTop() > about.offset().top - 100){
+        if ($(window).scrollTop() > biography.offset().top - 100){
             $('#my-name').removeClass('invisible').addClass('fade-in');
         }
     });
